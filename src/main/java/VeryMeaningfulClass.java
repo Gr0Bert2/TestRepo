56a000932a6e8cc443e39b1b471b183c86c01ebf
robert.gubin <gubinrobert@gmail.com>
2014-11-02 17:24:55 +0600

    Initial commit.

---
+++ src/main/java/VeryMeaningfulClass.java
@@ -0,0 +1,16 @@
+/**
+ * Created by robert on 02.11.14.
+ */
+public class VeryMeaningfulClass {
+    private int a;
+    private int b;
+
+    public VeryMeaningfulClass(final int a, final int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getSum() {
+        return a + b;
+    }
+}
---
+++ src/test/java/VeryMeaningfulClassTest.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+/**
+ * Created by robert on 02.11.14.
+ */
+public class VeryMeaningfulClassTest {
+
+    protected VeryMeaningfulClass veryMeaningfulClass;
+
+    @BeforeTest
+    public void init() {
+        veryMeaningfulClass = new VeryMeaningfulClass(5, 5);
+    }
+
+    @Test
+    public void getSumTest() {
+        Assert.assertEquals(veryMeaningfulClass.getSum(), 10);
+    }
+}
